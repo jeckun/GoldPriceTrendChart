8193b92c4f85309e80197ad639547099efeb98c4
jeckun <sz.eric.yang@qq.com>
2021-02-13 16:47:54 +0800

    update

---
+++ .history/djGlod/views_20210213163809.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+
+def runoob(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'index.html', context)
